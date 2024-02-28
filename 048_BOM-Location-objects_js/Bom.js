b02465f156eac7c4c48a80f8a48d241ae1d78a33
A H Shobuj <ahshobuj09@gmail.com>
2024-02-28 20:16:18 +0600

    Added BOM (Browser Object Model)

---
+++ 048_BOM-Location-objects_js/Bom.js
@@ -0,0 +1,45 @@
+//? BOM (Browser object model) 
+//* window object
+//* location object
+console.log(window);
+console.log(window.location);
+
+//* href
+console.log(location.href);
+
+//* host
+console.log(location.host);
+
+//* hostname
+console.log(location.hostname);
+
+//* port
+console.log(location.port);
+
+//* pathname
+console.log(location.pathname);
+
+//* protocol
+console.log(location.protocol);
+
+//* origin 
+console.log(location.origin);
+
+
+
+let locationDiv = document.querySelector('#location_div');
+
+let p1 = locationDiv.children[0];
+p1.innerHTML = location.href ;
+
+let p2 = p1.nextElementSibling ;
+p2.textContent = location.protocol ;
+
+let p3 = p2.nextElementSibling ;
+p3.innerText = location.host ; 
+
+let p4 = p3.nextElementSibling ;
+p4.innerText = location.hostname ; 
+
+let p5 = p4.nextElementSibling ;
+p5.innerText = location.port ; 
