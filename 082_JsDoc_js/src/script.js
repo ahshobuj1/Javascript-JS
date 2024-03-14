9b67b65d01814788dda8e8f9a87843f67a27b17f
A H Shobuj <ahshobuj09@gmail.com>
2024-03-15 05:42:17 +0600

    JsDoc added - jsdoc Documentation

--- 082_JsDoc_js/src/script.js
+++ 082_JsDoc_js/src/script.js
@@ -3,14 +3,21 @@ const formInput = document.querySelector(`#input_todo`);
 const todoList = document.querySelector(`.list`);
 const message = document.querySelector(`.todo_message`);
 
-//*! add todo in localStorage
+/**
+ * Get todo data from localStorage
+ * @returns {Array<string>} An empty Array | anything what does it have
+ */
 const addTodoLocalStorage = () => {
     return localStorage.getItem(`myTodo`)
         ? JSON.parse(localStorage.getItem(`myTodo`))
         : [];
 };
 
-//*! todo message
+/**
+ * It's show message when added or Delete TodoList
+ * @param {string} text It's receive text message
+ * @param {string} status It calls class
+ */
 const todoMessage = (text, status) => {
     message.innerHTML = text;
     message.classList.add(`message_${status}`);
@@ -21,9 +28,15 @@ const todoMessage = (text, status) => {
     }, 2000);
 };
 
-//*! create todo list
+/**
+ * Create li list Element , then add id-class and set inner html value ,maintenance Delete button as well
+ * @param {string} todoInputValue Todo input value from user
+ * @param {number} todoUniqueId This is todo Unique Id
+ */
 const createTodo = (todoInputValue, todoUniqueId) => {
-    //* Create li list element
+    /**
+     * Create li list Element
+     */
     const createTodoList = document.createElement(`li`);
     createTodoList.id = todoUniqueId;
     createTodoList.classList.add(`list_style`);
@@ -35,47 +48,72 @@ const createTodo = (todoInputValue, todoUniqueId) => {
 
     todoList.appendChild(createTodoList);
 
-    //* delete button
+    /**
+     * Find delete Button and addEvenListener
+     *@type{boolean}
+     */
     const deleteButton = createTodoList.querySelector(`#delButton`);
 
     deleteButton.addEventListener(`click`, deleteList);
 };
 
-//*! delete todo list
+/**
+ * Deleted todo list
+ * @param {EventTarget} event
+ */
 
 const deleteList = (event) => {
+    /**
+     * Find parent Element of deleteButton
+     */
     const selectList = event.target.parentElement.parentElement.parentElement;
 
     todoList.removeChild(selectList);
     todoMessage(`List deleted successfully`, `remove`);
 
-    //* delete todo from localStorage
+    /**
+     * Remove deleted Todo id from localStorage. Store again all todo data except deleted todo id
+     */
     let store = addTodoLocalStorage();
     store = store.filter((todo) => todo.todoUniqueId !== selectList.id);
     localStorage.setItem(`myTodo`, JSON.stringify(store));
 };
 
-//*! add event listener
+/**
+ * addEventListener with form to get user input data
+ * @param {Event} event
+ */
 const addForm = (event) => {
     event.preventDefault();
     const todoInputValue = formInput.value;
 
-    //* Create Unique id
+    /**
+     * Generate unique Id with Date.now() Function
+     */
     const todoUniqueId = Date.now().toString();
 
     createTodo(todoInputValue, todoUniqueId);
     todoMessage(`Added successfully`, `added`);
 
     //* added todo in localStorage
+    /**
+     * Set Todo Id and Value in localStorage
+     * @type{string}
+     */
     const store = addTodoLocalStorage();
 
     store.push({ todoUniqueId, todoInputValue });
     localStorage.setItem(`myTodo`, JSON.stringify(store));
 };
 
-//*! todo loaded from localStorage
+/**
+ * Loaded todo data from localStorage, After Refresh data will be still available
+ */
 
 const loadFromLocalStorage = () => {
+    /**
+     * Get todo data as Array and then map
+     */
     const getTodo = addTodoLocalStorage();
 
     getTodo.map((todo) => {
